5e03d93cdf87eb591580243dbaa53572be6bf04e
Lukas <lukas.haertinger@gmail.com>
2020-10-27 17:06:18 +0100

    added URL

--- src/Think_Positive_Umwandler.java
+++ src/Think_Positive_Umwandler.java
@@ -1,24 +1,25 @@
 import java.io.*;
+import java.net.MalformedURLException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.net.URL;
 
 public class Think_Positive_Umwandler {
     public static final int PORT=8082;
     public static final String HTML_DOC="<html> Hi wie gehts</html>";
-    public Think_Positive_Umwandler(){
+    private String URL;
+    public Think_Positive_Umwandler(String URL){
+        this.URL = URL;
         try(ServerSocket server = new ServerSocket(PORT)){
             try(Socket s = server.accept()){
                 BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(s.getOutputStream()));
                 BufferedReader br = new BufferedReader(new InputStreamReader(s.getInputStream()));
 
-                //read HTTP Request
-                for(String line = br.readLine();!line.isEmpty(); line = br.readLine()) {
-                    System.out.println(line);
-                }
                 //header
                 bw.write("HTTP/1.1 200 OK\r\n");
                 bw.write("\r\n");
-                bw.write(HTML_DOC);
+                //body
+                bw.write(getWebsite(URL));
                 bw.close();
 
             } catch (IOException ex){
@@ -28,8 +29,21 @@ public class Think_Positive_Umwandler {
             System.out.println("Server beendet");
         }
     }
-    String getWebsite(){
-        return null;
+    String getWebsite(String urlString){
+        String res= "";
+        try(InputStream input = new URL(urlString).openStream()){
+            BufferedReader br = new BufferedReader(new InputStreamReader(input));
+            System.out.println("\n received:");
+            for(String line = br.readLine();!line.isEmpty(); line = br.readLine()) {
+                res +=  line;
+                System.out.println(line);
+            }
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return res;
     }
 
     String modifyHTML(String original){
@@ -37,6 +51,6 @@ public class Think_Positive_Umwandler {
     }
 
     public static void main(String[] args) {
-        new Think_Positive_Umwandler();
+        new Think_Positive_Umwandler(args[0]);
     }
 }
