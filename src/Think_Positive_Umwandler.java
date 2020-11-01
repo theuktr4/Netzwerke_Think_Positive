a6989e80db6593a57991cafe723d9715d02488ff
hm-haerting <lukas.haertinger@hm.edu>
2020-11-01 12:16:03 +0100

    added request überprüfung

--- src/Think_Positive_Umwandler.java
+++ src/Think_Positive_Umwandler.java
@@ -7,6 +7,7 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.regex.Pattern;
 
 public class Think_Positive_Umwandler {
     private static final int PORT = 8082;
@@ -21,19 +22,30 @@ public class Think_Positive_Umwandler {
                 try (Socket s = server.accept()) {
                     System.out.println("Client connected");
                     BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(s.getOutputStream(), ENCODING));
-                    //Verbindung zur übergebenen Webseite
-                    URL urlObj = new URL(protocol + "://" + URL);
-                    URLConnection targetHost = urlObj.openConnection();
-                    //Body ermitteln
-                    String body = getWebsiteBody(targetHost);
-                    //Content Length berechnen
-                    int contentLength = body.getBytes(ENCODING).length;
-                    //Header von Zielwebseite an Client senden
-                    bw.write(getHeaderAsString(getwebsiteResponseheader(targetHost, contentLength)));
-                    bw.flush();
-                    //Webseite an Client senden
-                    bw.write(body);
-                    bw.close();
+                    BufferedReader br = new BufferedReader(new InputStreamReader(s.getInputStream()));
+                    //Einlesen des HTTP Requests
+                    String request = br.readLine();
+                    if(!request.contains("GET")){
+                        bw.write("HTTP/1.1 405 Method Not Allowed\n\r");
+                        bw.write("\n\r");
+                        bw.write("<html><body><h>405 Method Not Allowed</h></body></html>");
+                        bw.flush();
+                    }
+                    else {
+                        //Verbindung zur übergebenen Webseite
+                        URL urlObj = new URL(protocol + "://" + URL);
+                        URLConnection targetHost = urlObj.openConnection();
+                        //Body ermitteln
+                        String body = getWebsiteBody(targetHost);
+                        //Content Length berechnen
+                        int contentLength = body.getBytes(ENCODING).length;
+                        //Header von Zielwebseite an Client senden
+                        bw.write(getHeaderAsString(getwebsiteResponseheader(targetHost, contentLength)));
+                        bw.flush();
+                        //Webseite an Client senden
+                        bw.write(body);
+                        bw.flush();
+                    }
 
                 } catch (IOException ex) {
                     System.out.println("Client disconnected");
@@ -46,9 +58,7 @@ public class Think_Positive_Umwandler {
 
     String getWebsiteBody(URLConnection conn) {
         String res = "";
-        try {
-            InputStream input = conn.getInputStream();
-            BufferedReader br = new BufferedReader(new InputStreamReader(input,ENCODING));
+        try (BufferedReader br = new BufferedReader(new InputStreamReader(conn.getInputStream(),ENCODING))){
             System.out.println("\n received:");
             //HTML einlesen
             for (String line = br.readLine(); line != null; line = br.readLine()) {
@@ -59,7 +69,7 @@ public class Think_Positive_Umwandler {
         } catch (MalformedURLException e) {
             e.printStackTrace();
         } catch (IOException e) {
-            System.out.println("Verbindung zum TH fehlgeschlagen");
+            System.out.println("Verbindung zum TH abgebrochen");
         }
         System.out.println("body: " + res);
         return res;
@@ -98,16 +108,20 @@ public class Think_Positive_Umwandler {
     }
 
     String modifyLine(String line) {
-        ;
         String[] keywords = {"KI", "Maschinelles Lernen", "Java", "Computer", "MMIX", "RISC", "CISC", "Debugger", "Informatik", "Student", "Studentin", "Studierende", "Windows", "Linux", "Software", "Informtiker", "infomatikerInnen", "informatikerin"};
         for (String s : keywords) {
             line = line.replaceAll(s, s + "(Yeah)");
         }
+        //Pattern imageClass = Pattern.compile("\"\\s.*\"");
         line = line.replaceAll("<img\\s.*>", "<img src =https://upload.wikimedia.org/wikipedia/commons/8/8d/Smiley_head_happy.svg>");
         return line;
     }
 
     public static void main(String[] args) {
+        if(args.length<2){
+            System.err.println("Zu wenige Argumente übergeben: expected String<protocol>(http,https) String<URL>");
+            System.exit(1);
+        }
         new Think_Positive_Umwandler(args[0], args[1]);
     }
 }
